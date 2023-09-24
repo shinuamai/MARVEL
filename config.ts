09acdb96018638d5b89524176ad3511da4927d7e
shinuamai <laurad00sandovalg@email.com>
2023-09-24 17:54:13 -0500

    config info

---
+++ config.ts
@@ -0,0 +1,8 @@
+// config.ts
+
+export const environment = {
+    production: false,
+    marvelApiKey: '9bc3a50ac51820c006543e9be325f09c',
+    marvelApiHash: 'da91a5ee894dec82a20f691a292e747d',
+  };
+  
